d84ceefca7b5dbbb488a0d5b5fae986bde3f70c3
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-11 11:27:33 +0000

    added basic.cpp and fundamental.cpp

---
+++ fundamental/basic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "provide me a no. and i will giv you a sum of it" << endl;
+
+  int a, b;
+  cout << "input first arg" << endl;
+  cin >> a;
+  cout << "input second arg" << endl;
+  cin >> b;
+  cout << "sum of provided arg is " << a + b << endl;
+  return 0;
+}
