4c31f04e0d49848149b5f497f7169bda9116dabf
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-11 12:39:16 +0000

    4Q print star's in right-angle triangle formate

--- fundamental/pattern.cpp
+++ fundamental/pattern.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 // 3Q print patter for n such that it goes from 1 to n in +x and +y direction
+// int main() {
+
+//   cout << "let me print a pattern for you just give me the no. " << endl;
+//   int n;
+
+//   cin >> n;
+
+//   for (int i = 1; i <= n; i++) {
+
+//     for (int j = 1; j <= n; j++) {
+//       cout << j << " ";
+//     }
+//     cout << endl;
+//   }
+// }
+// 4Q print star's in right-angle triangle formate
 int main() {
 
-  cout << "let me print a pattern for you just give me the no. " << endl;
   int n;
-
   cin >> n;
 
-  for (int i = 1; i <= n; i++) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
 
-    for (int j = 1; j <= n; j++) {
-      cout << j << " ";
+      cout << "*"
+           << " ";
     }
     cout << endl;
   }
