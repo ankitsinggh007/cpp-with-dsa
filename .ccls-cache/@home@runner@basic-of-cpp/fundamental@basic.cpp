9c8b6bf05ee6777bb84b8d046aff7b9dfaaa3ae6
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-11 11:56:30 +0000

    2Q find factorial of n?

--- .ccls-cache/@home@runner@basic-of-cpp/fundamental@basic.cpp
+++ .ccls-cache/@home@runner@basic-of-cpp/fundamental@basic.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
+// int main() {
 
-  cout << "provide me a no. and i will giv you a sum of it" << endl;
+//   cout << "provide me a no. and i will giv you a sum of it" << endl;
 
-  int a, b;
-  cout << "input first arg" << endl;
-  cin >> a;
-  cout << "input second arg" << endl;
-  cin >> b;
-  cout << "sum of provided arg is " << a + b << endl;
-  return 0;
-}
+//   int a, b;
+//   cout << "input first arg" << endl;
+//   cin >> a;
+//   cout << "input second arg" << endl;
+//   cin >> b;
+//   cout << "sum of provided arg is " << a + b << endl;
+//   return 0;
+// }
--- fundamental/homeword.cpp
+++ fundamental/homeword.cpp
@@ -1,18 +1,44 @@
 // ////////////////////////////////
 // 1Q print all odd no.from 1 to n (including both)
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int a;
+//   cout << "give me a number and i will print all no. from 1 to that number"
+//        << endl;
+//   cin >> a;
+//   for (int i = 0; i <= a; i++) {
+//     if (i % 2 != 0) {
+//       cout << i << endl;
+//     }
+//   }
+//   return 0;
+// }
+// ////////////////////////////////
+// 2Q find factorial of n?
+  #include <iostream>
 using namespace std;
 
-int main() {
-  int a;
-  cout << "give me a number and i will print all no. from 1 to that number"
-       << endl;
-  cin >> a;
-  for (int i = 0; i <= a; i++) {
-    if (i % 2 != 0) {
-      cout << i << endl;
-    }
+int main (){
+
+  int n;
+
+  cout<<"give an input so that i can give you factorial of that input "<<endl;
+  cin>>n;
+  int i=n;
+  cout<<i;
+  int ans=1;
+  while(i>=1){
+  ans=ans*i;
+    i--;
   }
-  return 0;
-}
+  if(n>=0){
+    cout<<"answer"<<endl<<ans;
+  }
+  else{
+    cout<<"answer"<<endl<<0;
+  }
+  }
+
