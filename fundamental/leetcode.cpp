106cbf2380188c82e9ff1d952b3874b9d260f16b
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-14 13:43:03 +0000

    Given an integer n, return true if it is a power of two. Otherwise, return false.leet code easy

--- fundamental/leetcode.cpp
+++ fundamental/leetcode.cpp
@@ -1,26 +1,45 @@
 // Reverse int
+// class Solution {
+// public:
+//     int reverse(int x) {
+
+//         int ans=0;
+//         int i=0;
+//         while(x!=0){
+
+//             int LastDigit=x%10;
+//             x=x/10;
+//             if(ans>INT_MAX/10 || ans<INT_MIN/10 ){
+//                 return 0;
+
+//             }
+//             else{
+//             ans=ans*10+LastDigit;
+//             i++;
+
+//             }
+
+//         }
+//         return ans;
+//     }
+// };
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
 class Solution {
 public:
-    int reverse(int x) {
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+
+while(n!=0){
+
+    if(!(n&1){
+        return false;
+    }
+    n=n>>1;
+    
+}
+return true;
         
-        int ans=0;
-        int i=0;
-        while(x!=0){
-
-            int LastDigit=x%10;
-            x=x/10;
-            if(ans>INT_MAX/10 || ans<INT_MIN/10 ){
-                return 0;
-
-            }
-            else{
-            ans=ans*10+LastDigit;
-            i++;
-
-            }
-            
-        
-        }
-        return ans;    
     }
 };
