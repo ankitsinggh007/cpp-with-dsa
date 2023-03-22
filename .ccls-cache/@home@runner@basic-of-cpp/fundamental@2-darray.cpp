1d6c1e046980160529667780b02840f8d78869a0
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-22 18:03:12 +0000

    mdification

--- .ccls-cache/@home@runner@basic-of-cpp/fundamental@2-darray.cpp
+++ .ccls-cache/@home@runner@basic-of-cpp/fundamental@2-darray.cpp
@@ -1,43 +1,57 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-void getValue(int arr[][4],int row,int col){
+// void getValue(int arr[][4],int row,int col){
 
-  for(int i=0;i<row;i++){
-    for(int j=0;j<col;j++){
-      cin>>arr[i][j];
-    }
+//   for(int i=0;i<row;i++){
+//     for(int j=0;j<col;j++){
+//       cin>>arr[i][j];
+//     }
+//   }
+// }
+// void printArray(int arr[][4],int row,int col){
+//   for(int i=0;i<row;i++){
+//     for(int j=0;j<col;j++){
+//       cout<<arr[i][j];
+//     }
+//     cout<<endl;
+//   }
+// }
+// void largestRowSum(int arr[][4],int row,int col){
+//   pair<int,int>temp;
+//   for(int i=0;i<row;i++){
+//     int sum=0;
+//     for(int j=0;j<col;j++){
+//       sum+=arr[i][j];
+//     }
+//     if(temp.first<sum){
+//       temp.first=sum;
+//       temp.second=i;
+//     }
+//   }
+//   cout<<"this row have maximum sum"<<temp.second;
+// }
+vector<int> findPair(int x, int y) {
+  // Write your code here.
+  int X = (x + y) / 2;
+  int Y = (x - y) / 2;
+  vector<int> v;
+  cout<<"size"<<sizeof(X)<<"size"<<endl;
+  if (sizeof(X) == 4 && sizeof(Y) == 4) {
+    v.push_back(X);
+    v.push_back(Y);
+  } else {
+    v.push_back(-1);
+    v.push_back(-1);
   }
+  return v;
 }
-void printArray(int arr[][4],int row,int col){
-  for(int i=0;i<row;i++){
-    for(int j=0;j<col;j++){
-      cout<<arr[i][j];
-    }
-    cout<<endl;
-  }
-}
-void largestRowSum(int arr[][4],int row,int col){
-  pair<int,int>temp;
-  for(int i=0;i<row;i++){
-    int sum=0;
-    for(int j=0;j<col;j++){
-      sum+=arr[i][j];
-    }
-    if(temp.first<sum){
-      temp.first=sum;
-      temp.second=i;
-    }
-  }
-  cout<<"this row have maximum sum"<<temp.second;
-}
-int main(){
+int main() {
 
-  cout<<"provide me a size of row the col will alway's be same"  <<endl;
-  int row;
-  cin>>row;
-  int arr[row][4];
-  getValue(arr,row,4);
-  cout<<endl;
-  printArray(arr,row,4);
-  largestRowSum(arr,row,4);
+  cout << "provide me a size of row the col will alway's be same" << endl;
+  int x, y;
+  cin >> x>>y;
+  cout<<"input"<<x<<" "<<y<<"input"<<endl;
+  vector<int> v=findPair(x, y);
+  cout<<v[0]<<" "<<v[1];
 }
