b36f77a1687b77d05807b72ea511cd308b973c3c
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-24 16:50:33 +0000

    pointer and function

--- .ccls-cache/@home@runner@basic-of-cpp/fundamental@Pointer.cpp
+++ .ccls-cache/@home@runner@basic-of-cpp/fundamental@Pointer.cpp
@@ -1,11 +1,16 @@
-           /////////////////////////////////////////////Pointer///////////////////////////////////////////////////////////////
+/////////////////////////////////////////////Pointer///////////////////////////////////////////////////////////////
 
 #include <iostream>
 using namespace std;
+void printArray(int a[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+}
+int main() {
+int arr[10]={1,2,3,4,5,6};
 
-int main(){
-char ch='a';
-  cout<<ch<<endl;
-  ch++;
-  cout<<ch;
+  int * p=arr;
+  int *q=arr+3;
+  cout<<*p<<endl<<*q<<endl<<q-p;
 }
