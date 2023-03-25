f245b6a0b46a854bb084a702a926916f647caf87
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-25 17:25:13 +0000

    Reference value

--- fundamental/Pointer.cpp
+++ fundamental/Pointer.cpp
@@ -7,10 +7,22 @@ void printArray(int a[], int n) {
     cout << a[i] << " ";
   }
 }
-int main() {
-int arr[10]={1,2,3,4,5,6};
+// int main() {
+// int arr[10]={1,2,3,4,5,6};
 
-  int * p=arr;
-  int *q=arr+3;
-  cout<<*p<<endl<<*q<<endl<<q-p;
+//   int * p=arr;
+//   int *q=arr+3;
+//   cout<<*p<<endl<<*q<<endl<<q-p;
+// }
+// Refrence value
+
+int main() {
+  int i = 10;
+  int &j = i;
+  cout << i << endl;
+  cout << j << endl;
+  i++;
+  j++;
+  cout << i << endl;
+  cout << j << endl;
 }
