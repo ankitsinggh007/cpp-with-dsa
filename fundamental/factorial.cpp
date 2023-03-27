4244c60029f9e6a519168ddb7f6d3719e2aa3c72
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-27 04:59:45 +0000

    find exponant

--- fundamental/factorial.cpp
+++ fundamental/factorial.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 // recursion
 
 // find factorialof n?
-int factorial(int n){
-  if(n==0||n==1){
+int factorial(int n) {
+  if (n == 0 || n == 1) {
     return 1;
-  }
-  else{
-    return n*factorial(n-1);
+  } else {
+    return n * factorial(n - 1);
   }
 }
-int main(){
-  int n;
-  cout<<"Provide n"<<endl;
-  cin>>n;
-cout<<factorial(n)<<endl;
-  
+int Exponant(int n, int i) {
+  if(i==0) return 1;
+  return n*Exponant(n,i-1);
+}
+int main() {
+  int n, i;
+  cout << "Provide n and i" << endl;
+  cin >> n >> i;
+  // cout<<factorial(n)<<endl;
+  cout << Exponant(n, i) << endl;
 }
