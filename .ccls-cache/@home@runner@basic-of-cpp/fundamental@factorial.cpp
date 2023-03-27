5b74ac56c273ad256b13c095dd534bcbe314b6bb
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-03-27 05:22:35 +0000

    Print Fibonacci no.

--- .ccls-cache/@home@runner@basic-of-cpp/fundamental@factorial.cpp
+++ .ccls-cache/@home@runner@basic-of-cpp/fundamental@factorial.cpp
@@ -20,11 +20,16 @@ void PrintCount(int n){
   if(n==1) return;
   return PrintCount(n-1);
 }
+int PrintFibonacci(int n){
+  if(n==0|n==1) return n;
+  else return PrintFibonacci(n-1)+PrintFibonacci(n-2);
+}
 int main() {
   int n, i;
   cout << "Provide n and i" << endl;
-  cin >> n >> i;
+  cin >> n ;
   // cout<<factorial(n)<<endl;
   // cout << Exponant(n, i) << endl;
-  PrintCount(n);
+  // PrintCount(n);
+  cout<<PrintFibonacci(n);
 }
