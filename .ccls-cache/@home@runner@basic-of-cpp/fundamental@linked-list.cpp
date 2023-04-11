1ee78c397c616103ec6f992b7008dae27940bbca
Ankitsingh56 <8622085-Ankitsingh56@users.noreply.replit.com>
2023-04-11 18:04:43 +0000

    reverse a linkedlist

--- .ccls-cache/@home@runner@basic-of-cpp/fundamental@linked-list.cpp
+++ .ccls-cache/@home@runner@basic-of-cpp/fundamental@linked-list.cpp
@@ -1,52 +1,52 @@
-#include <iostream>
-using namespace std;
-class Node {
-public:
-  int data;
-  Node *next;
-};
+// #include <iostream>
+// using namespace std;
+// class Node {
+// public:
+//   int data;
+//   Node *next;
+// };
 
-void insertAtHead(Node *&head, int n) {
+// void insertAtHead(Node *&head, int n) {
 
-  Node *temp = new Node;
-  temp->data = n;
-  temp->next = head;
-  head = temp;
-}
-void insertAtTail(Node *&head, int n) {
+//   Node *temp = new Node;
+//   temp->data = n;
+//   temp->next = head;
+//   head = temp;
+// }
+// void insertAtTail(Node *&head, int n) {
 
-  Node *tail = head;
-  if (tail == NULL) {
-    Node *temp = new Node;
-    temp->data = n;
-    temp->next = head;
-    head = temp;
-    return;
-  }
+//   Node *tail = head;
+//   if (tail == NULL) {
+//     Node *temp = new Node;
+//     temp->data = n;
+//     temp->next = head;
+//     head = temp;
+//     return;
+//   }
 
-  while (tail->next != NULL) {
-    tail = tail->next;
-  }
+//   while (tail->next != NULL) {
+//     tail = tail->next;
+//   }
 
-  Node *temp = new Node;
-  temp->data = n;
-  temp->next = NULL;
-  tail->next = temp;
-}
-void print(Node *head) {
+//   Node *temp = new Node;
+//   temp->data = n;
+//   temp->next = NULL;
+//   tail->next = temp;
+// }
+// void print(Node *head) {
 
-  while (head != NULL) {
-    cout << head->data << " ";
-    head = head->next;
-  }
-}
+//   while (head != NULL) {
+//     cout << head->data << " ";
+//     head = head->next;
+//   }
+// }
 
-int main() {
+// int main() {
 
-  Node *head = NULL;
-  insertAtTail(head, 7);
-  insertAtTail(head, 5);
-  insertAtTail(head, 2);
-  insertAtTail(head, 0);
-  print(head);
-}
+//   Node *head = NULL;
+//   insertAtTail(head, 7);
+//   insertAtTail(head, 5);
+//   insertAtTail(head, 2);
+//   insertAtTail(head, 0);
+//   print(head);
+// }
