cf24d811343975be4aa0a94c828eeaae2caa4554
ankitsinggh007 <46644970+ankitsinggh007@users.noreply.github.com>
2023-04-12 14:25:20 +0000

    detenet and remove cycle in ll

--- fundamental/linked-list.cpp
+++ fundamental/linked-list.cpp
@@ -50,3 +50,85 @@
 //   insertAtTail(head, 0);
 //   print(head);
 // }
+#include <bits/stdc++.h>
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+                int data;
+                Node *next;
+                Node(int data)
+                {
+                        this->data = data;
+                        this->next = NULL;
+                }
+        };
+
+*****************************************************************/
+
+Node *floydDetection(Node *head) {
+  if (head == NULL) {
+    return NULL;
+  }
+
+  Node *slow = head;
+  Node *fast = head;
+
+  while (slow != NULL and fast != NULL) {
+
+    fast = fast->next;
+
+    if (fast != NULL) {
+      fast = fast->next;
+    }
+
+    slow = slow->next;
+
+    if (slow == fast) {
+      return slow;
+    }
+  }
+  return NULL;
+}
+
+Node *getStartingNode(Node *head) {
+
+  if (head == NULL) {
+    return NULL;
+  }
+
+  Node *intersection = floydDetection(head);
+
+  if (intersection == NULL) {
+    return NULL;
+  }
+
+  Node *slow = head;
+
+  while (slow != intersection) {
+    slow = slow->next;
+    intersection = intersection->next;
+  }
+  return slow;
+}
+
+bool detectAndRemoveCycle(Node *head) {
+  // Write your code here
+  if (head == NULL) {
+    return NULL;
+  }
+  Node *startOfLoop = getStartingNode(head);
+  if (startOfLoop == NULL) {
+    return false;
+  }
+
+  Node *temp = startOfLoop;
+
+  while (temp->next != startOfLoop) {
+    temp = temp->next;
+  }
+  temp->next = NULL;
+}
