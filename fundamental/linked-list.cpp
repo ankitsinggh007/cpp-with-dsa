f2ab5e37544526b065f82c30630f192f936d5bae
ankit singh <46644970+ankitsinggh007@users.noreply.github.com>
2023-04-13 15:54:59 +0000

    redo

--- fundamental/linked-list.cpp
+++ fundamental/linked-list.cpp
@@ -1,182 +1,182 @@
-// // // #include <iostream>
-// // // using namespace std;
-// // // class Node {
-// // // public:
-// // //   int data;
-// // //   Node *next;
-// // // };
+// // #include <iostream>
+// // using namespace std;
+// // class Node {
+// // public:
+// //   int data;
+// //   Node *next;
+// // };
 
-// // // void insertAtHead(Node *&head, int n) {
+// // void insertAtHead(Node *&head, int n) {
 
-// // //   Node *temp = new Node;
-// // //   temp->data = n;
-// // //   temp->next = head;
-// // //   head = temp;
-// // // }
-// // // void insertAtTail(Node *&head, int n) {
-
-// // //   Node *tail = head;
-// // //   if (tail == NULL) {
-// // //     Node *temp = new Node;
-// // //     temp->data = n;
-// // //     temp->next = head;
-// // //     head = temp;
-// // //     return;
-// // //   }
-
-// // //   while (tail->next != NULL) {
-// // //     tail = tail->next;
-// // //   }
-
-// // //   Node *temp = new Node;
-// // //   temp->data = n;
-// // //   temp->next = NULL;
-// // //   tail->next = temp;
-// // // }
-// // // void print(Node *head) {
-
-// // //   while (head != NULL) {
-// // //     cout << head->data << " ";
-// // //     head = head->next;
-// // //   }
-// // // }
-
-// // // int main() {
-
-// // //   Node *head = NULL;
-// // //   insertAtTail(head, 7);
-// // //   insertAtTail(head, 5);
-// // //   insertAtTail(head, 2);
-// // //   insertAtTail(head, 0);
-// // //   print(head);
-// // // }
-// // #include <bits/stdc++.h>
-// // /****************************************************************
-
-// //     Following is the class structure of the Node class:
-
-// //         class Node
-// //         {
-// //         public:
-// //                 int data;
-// //                 Node *next;
-// //                 Node(int data)
-// //                 {
-// //                         this->data = data;
-// //                         this->next = NULL;
-// //                 }
-// //         };
-
-// // *****************************************************************/
-
-// // Node *floydDetection(Node *head) {
-// //   if (head == NULL) {
-// //     return NULL;
-// //   }
-
-// //   Node *slow = head;
-// //   Node *fast = head;
-
-// //   while (slow != NULL and fast != NULL) {
-
-// //     fast = fast->next;
-
-// //     if (fast != NULL) {
-// //       fast = fast->next;
-// //     }
-
-// //     slow = slow->next;
-
-// //     if (slow == fast) {
-// //       return slow;
-// //     }
-// //   }
-// //   return NULL;
+// //   Node *temp = new Node;
+// //   temp->data = n;
+// //   temp->next = head;
+// //   head = temp;
 // // }
+// // void insertAtTail(Node *&head, int n) {
 
-// // Node *getStartingNode(Node *head) {
-
-// //   if (head == NULL) {
-// //     return NULL;
+// //   Node *tail = head;
+// //   if (tail == NULL) {
+// //     Node *temp = new Node;
+// //     temp->data = n;
+// //     temp->next = head;
+// //     head = temp;
+// //     return;
 // //   }
 
-// //   Node *intersection = floydDetection(head);
-
-// //   if (intersection == NULL) {
-// //     return NULL;
+// //   while (tail->next != NULL) {
+// //     tail = tail->next;
 // //   }
 
-// //   Node *slow = head;
-
-// //   while (slow != intersection) {
-// //     slow = slow->next;
-// //     intersection = intersection->next;
-// //   }
-// //   return slow;
-// // }
-
-// // bool detectAndRemoveCycle(Node *head) {
-// //   // Write your code here
-// //   if (head == NULL) {
-// //     return NULL;
-// //   }
-// //   Node *startOfLoop = getStartingNode(head);
-// //   if (startOfLoop == NULL) {
-// //     return false;
-// //   }
-
-// //   Node *temp = startOfLoop;
-
-// //   while (temp->next != startOfLoop) {
-// //     temp = temp->next;
-// //   }
+// //   Node *temp = new Node;
+// //   temp->data = n;
 // //   temp->next = NULL;
+// //   tail->next = temp;
 // // }
-// bool detectCycle(Node *head) {
+// // void print(Node *head) {
+
+// //   while (head != NULL) {
+// //     cout << head->data << " ";
+// //     head = head->next;
+// //   }
+// // }
+
+// // int main() {
+
+// //   Node *head = NULL;
+// //   insertAtTail(head, 7);
+// //   insertAtTail(head, 5);
+// //   insertAtTail(head, 2);
+// //   insertAtTail(head, 0);
+// //   print(head);
+// // }
+// #include <bits/stdc++.h>
+// /****************************************************************
+
+//     Following is the class structure of the Node class:
+
+//         class Node
+//         {
+//         public:
+//                 int data;
+//                 Node *next;
+//                 Node(int data)
+//                 {
+//                         this->data = data;
+//                         this->next = NULL;
+//                 }
+//         };
+
+// *****************************************************************/
+
+// Node *floydDetection(Node *head) {
+//   if (head == NULL) {
+//     return NULL;
+//   }
 
 //   Node *slow = head;
 //   Node *fast = head;
 
-//   while (slow != NULL && fast != NULL && fast->next != NULL) {
-//     fast = fast->next->next;
+//   while (slow != NULL and fast != NULL) {
+
+//     fast = fast->next;
+
+//     if (fast != NULL) {
+//       fast = fast->next;
+//     }
+
 //     slow = slow->next;
+
 //     if (slow == fast) {
-//       return true;
+//       return slow;
 //     }
 //   }
-//   return false;
+//   return NULL;
 // }
-// /************************************************************
 
-//     Following is the linked list node structure.
+// Node *getStartingNode(Node *head) {
 
-//     class Node
-//     {
-//         public:
-//         int data;
-//         Node* next;
+//   if (head == NULL) {
+//     return NULL;
+//   }
 
-//         Node(int data)
-//         {
-//             this->data = data;
-//             this->next = NULL;
-//         }
-//     };
+//   Node *intersection = floydDetection(head);
 
-// ************************************************************/
+//   if (intersection == NULL) {
+//     return NULL;
+//   }
 
-// Node * uniqueSortedList(Node * head) {
-//    if(head==NULL) return head;
-//     Node*temp=head->next;
-//     Node*curr=head;
-//     while(temp!=NULL){
-//         if(curr->data==temp->data){
-//             temp=temp->next;
-//             curr->next=temp;
-//             continue;
-//         }
-//         curr=curr->next;
-//         temp=temp->next;
-//     }
-//     return head;
+//   Node *slow = head;
+
+//   while (slow != intersection) {
+//     slow = slow->next;
+//     intersection = intersection->next;
+//   }
+//   return slow;
 // }
+
+// bool detectAndRemoveCycle(Node *head) {
+//   // Write your code here
+//   if (head == NULL) {
+//     return NULL;
+//   }
+//   Node *startOfLoop = getStartingNode(head);
+//   if (startOfLoop == NULL) {
+//     return false;
+//   }
+
+//   Node *temp = startOfLoop;
+
+//   while (temp->next != startOfLoop) {
+//     temp = temp->next;
+//   }
+//   temp->next = NULL;
+// }
+bool detectCycle(Node *head) {
+
+  Node *slow = head;
+  Node *fast = head;
+
+  while (slow != NULL && fast != NULL && fast->next != NULL) {
+    fast = fast->next->next;
+    slow = slow->next;
+    if (slow == fast) {
+      return true;
+    }
+  }
+  return false;
+}
+/************************************************************
+
+    Following is the linked list node structure.
+
+    class Node
+    {
+        public:
+        int data;
+        Node* next;
+
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+************************************************************/
+
+Node * uniqueSortedList(Node * head) {
+   if(head==NULL) return head;
+    Node*temp=head->next;
+    Node*curr=head;
+    while(temp!=NULL){
+        if(curr->data==temp->data){
+            temp=temp->next;
+            curr->next=temp;
+            continue;
+        }
+        curr=curr->next;
+        temp=temp->next;
+    }
+    return head;
+}
