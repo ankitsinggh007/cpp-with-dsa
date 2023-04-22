0bb8e53e5e8a19131aaaab799cb4c0394f33fc5b
ankit singh <as3824115@gmai.com>
2023-04-22 23:47:56 +0530

    Delete middle eleent form stack

--- stack/stack-part-1.cpp
+++ stack/stack-part-1.cpp
@@ -4,26 +4,26 @@
 using namespace std;
 // Q1 Reverese a sstring using stack?
 
-void ReverseStr(string &str){
 
- stack<char>S;
-
- for(int i=0;i<str.size();i++){
-    char a=str[i];
-    S.push(a);
- }
-    string st="";
- for(int i=0;i<str.size();i++){
-  cout<<S.pop()<<endl;
- }
+#include <bits/stdc++.h>
+void solve(stack<int>&inputStack,int mid,int count){
+   if(count==mid){
+      inputStack.pop();
+      return;
+   }
+   int num=inputStack.top();
+   inputStack.pop();
+   solve(inputStack,mid,++count);
+      inputStack.push(num);
 
 }
-
+ 
+void deleteMiddle(stack<int>&inputStack, int N){
+      int mid=N/2;
+      int count=0;
+   solve(inputStack,mid,count);
+}
 int main(){
 
-string str="ankit";
-
-ReverseStr(str);
-cout<<"ans"<<" : "<<str<<endl;
 
 }
